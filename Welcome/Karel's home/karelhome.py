a1487442a1fa707d7f4b9fb0a8375c64bd674b11
Luna Estanga <yasestanga@gmail.com>
2026-06-05 19:04:13 -0300

    Update karelhome.py

--- Welcome/Karel's home/karelhome.py
+++ Welcome/Karel's home/karelhome.py
@@ -1,34 +1,35 @@
-#incomplete and i have to fix errors from  return home
 from karel.stanfordkarel import *
 
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
-
-def go_to_beeper():
-    turn_right()    # mira al sur
-    move()          # baja 1
-    turn_left()     # mira al este
-    move()          # derecha 1
-    move()          # sale por la puerta
-    move()          # llega al beeper
-
-def return_home():
-    turn_left()     # mira al norte
-    turn_left()     # mira al oeste
-    move()          # entra por la puerta
-    move() 
-    turn_left()
-    turn_right() 
-    move() 
-    turn_right()   # mira al este
-   
-
+# File: shelter.py
+# -----------------------------
+# The warmup program defines a "main"
+# function which should make Karel 
+# move to the beeper, pick it up, and
+# return home.
 def main():
-    go_to_beeper()
+    move()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    move()
     pick_beeper()
-    return_home()
-
+    turn_left()
+    turn_left()
+    move()
+    move()
+    move()
+    turn_right()
+    move()
+    turn_right()
+def turn_right():
+    for i in range(3):
+        turn_left()
+    # add your code here
+    
+    
+    
+# don't edit these next two lines
+# they tell python to run your main function
 if __name__ == '__main__':
     main()
