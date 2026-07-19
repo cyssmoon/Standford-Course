637822b78cafe4f19a52b09d3c161a096812fa3b
yun <yasestanga@gmail.com>
2026-07-18 23:45:14 -0300

    Create year.py

---
+++ Intro to Python/Dog years/year.py
@@ -0,0 +1,9 @@
+def main():
+    age = float(input("Enter an age in calendar years: "))
+    
+    dog_years = age * 7.18
+    
+    print("That's", dog_years, "in dog years!")
+
+if __name__ == '__main__':
+    main()
